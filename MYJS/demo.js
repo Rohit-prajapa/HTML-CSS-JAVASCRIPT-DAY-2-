5d52f4bfd58b627a8ca3601c0723942a3e84d1ff
Rohit Prajapati <rahulprajapati69424@gmail.com>
2026-06-01 15:03:08 +0530

    Add HTML project files

---
+++ MYJS/demo.js
@@ -0,0 +1,11 @@
+console.log("this is a demo javascript file.")
+
+doc=document.getElementById("xyz")
+console.log(doc)
+
+function myDemo(){
+    doc.textContent="Javascript is hard"
+    doc.style.color='red';
+    doc.style.textAlign='center'
+    doc.style.fontSize="43px"
+}
